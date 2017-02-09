6dc4e6951760d1038271c9678df7fc3549fa2c24
Piotr Srebniak <piotr.srebniak@gmail.com>
2017-02-09 21:40:12 +0100

    change

--- client/index.php
+++ client/index.php
@@ -8,6 +8,8 @@ class SocketException extends \Exception
 
 class Socket
 {
+    const EOL = "\r\n";
+
     protected $socket = null;
     protected $path = null;
 
@@ -15,8 +17,10 @@ class Socket
     protected $type = null;
     protected $protocol = null;
 
+    protected $debug = false;
+
     /**
-     * SocketWrapper constructor.
+     * Socket constructor.
      * @param int $domain socket_create $domain parameter
      * @param int $type socket_create $type parameter
      * @param int $protocol socket_create $protocol parameter
@@ -33,6 +37,14 @@ class Socket
     }
 
     /**
+     * Socket destructor, call close method
+     */
+    public function __destruct()
+    {
+        $this->close();
+    }
+
+    /**
      * Create new socket if not exist
      * Socket is created in constructor
      *
@@ -41,6 +53,10 @@ class Socket
      */
     public function create()
     {
+        if ($this->debug) {
+            echo __METHOD__ . PHP_EOL;
+        }
+
         if (!isset($this->socket)) {
             $this->socket = socket_create($this->domain, $this->type, $this->protocol);
             if (!isset($this->socket)) {
@@ -60,13 +76,17 @@ class Socket
      */
     public function connect($path)
     {
+        if ($this->debug) {
+            echo __METHOD__ . PHP_EOL;
+        }
+
         if (false === socket_connect($this->socket, $path)) {
             throw new \SocketException("Cannot connect socket to {$path}");
         }
-        if (false === socket_set_nonblock($this->socket)) {
-            $this->close();
-            throw new \SocketException("Cannot set socket as non blocking");
-        }
+//        if (false === socket_set_nonblock($this->socket)) {
+//            $this->close();
+//            throw new \SocketException("Cannot set socket as non blocking");
+//        }
 
         return $this;
     }
@@ -80,12 +100,16 @@ class Socket
      */
     public function write($payload)
     {
-        if (!isset($this->socket) ) {
-            throw new \SocketException("Cannot write to empty socket".PHP_EOL);
+        if ($this->debug) {
+            echo "> " . $payload;
+        }
+
+        if (!isset($this->socket)) {
+            throw new \SocketException("Cannot write to empty socket" . PHP_EOL);
         }
 
         if (false === socket_write($this->socket, $payload, strlen($payload))) {
-            throw new \SocketException("Error occur when write to stream".PHP_EOL);
+            throw new \SocketException("Error occur when write to stream" . PHP_EOL);
         }
 
         return $this;
@@ -99,17 +123,50 @@ class Socket
      * @return $this
      * @throws \SocketException
      */
-    public function send($payload, $flag = MSG_EOF)
+    public function send($payload, $flag = MSG_EOR)
     {
-        if (!isset($this->socket) ) {
-            throw new \SocketException("Cannot write to empty socket".PHP_EOL);
+        if ($this->debug) {
+            echo "> " . $payload;
+        }
+        if (!isset($this->socket)) {
+            throw new \SocketException("Cannot write to empty socket" . PHP_EOL);
         }
 
         if (false === socket_send($this->socket, $payload, strlen($payload), $flag)) {
-            throw new \SocketException("Error occur when send to stream".PHP_EOL);
+            throw new \SocketException("Error occur when send to stream" . PHP_EOL);
         }
 
         return $this;
+    }
+
+    /**
+     * Read from socket
+     *
+     * @param int $type socket_read $type parameter
+     *
+     * @return string
+     * @throws SocketException
+     */
+    public function read($type = PHP_BINARY_READ)
+    {
+        if ($this->debug) {
+            echo __METHOD__.PHP_EOL;
+        }
+        if (!isset($this->socket)) {
+            throw new \SocketException("Cannot read from empty socket" . PHP_EOL);
+        }
+
+        $response = "";
+        while (($partial = socket_read($this->socket, 65384, $type))) {
+            if (false === $partial) {
+                $last_error = socket_last_error($this->socket);
+                $message = socket_strerror($last_error);
+                throw new SocketException("Error occur when read from stream: {$message}", $last_error);
+            }
+            $response .= $partial;
+        }
+
+        return $response;
     }
 
     /**
@@ -119,12 +176,27 @@ class Socket
      */
     public function close()
     {
+        if ($this->debug) {
+            echo __METHOD__.PHP_EOL;
+        }
+
         if (isset($this->socket)) {
             socket_close($this->socket);
             $this->socket = null;
         }
 
         return $this;
+    }
+
+    /**
+     * Set debugging flag
+     * Debugger echoes methods calls
+     *
+     * @param bool $debug
+     */
+    public function setDebug($debug = true)
+    {
+        $this->debug = (bool)$debug;
     }
 }
 
@@ -154,86 +226,48 @@ class UnixSocketHandler
         if (isset($options['delay'])) {
             usleep($options['delay'] * 1000);
         }
-        $text = "";
+        $socket = new Socket($this->domain, $this->type, $this->protocol);
+        $socket->setDebug();
+        $socket->connect($this->path);
+        $socket->write("{$request->getMethod()} {$request->getRequestTarget()} HTTP/{$request->getProtocolVersion()}" . Socket::EOL);
 
-        $socket = $cre
-
-
-        $line = "{$request->getMethod()} {$request->getRequestTarget()} HTTP/{$request->getProtocolVersion()}" . PHP_EOL;
-        $text .= $line;
-        socket_write($socket, $line, strlen($line));
         foreach ($request->getHeaders() as $key => $values) {
             $value = implode(', ', $values);
-            $line = "{$key}: {$value}" . PHP_EOL;
-            $text .= $line;
-            socket_write($socket, $line, strlen($line));
+            $socket->write("{$key}: {$value}" . Socket::EOL);
         }
 
         $contentLength = strlen($request->getBody());
-//        if ($contentLength > 0) {
-//            $line = "Content-length: {$contentLength}" . PHP_EOL;
-//            $text .= $line;
-//            socket_write($socket, $line, strlen($line));
-//        }
-
-        $line = PHP_EOL;
-        $text .= $line;
-        socket_write($socket, $line, strlen($line));
-
-        $line = $request->getBody();
-        $text .= $line;
-        socket_write($socket, $line, strlen($line));
-
-        $line = PHP_EOL . PHP_EOL . PHP_EOL;
-        $text .= $line;
-        socket_send($socket, $line, strlen($line), MSG_EOF);
-
-        echo "---BEGIN---" . PHP_EOL;
-        echo $text;
-        echo PHP_EOL . "---END---";
-
-        $response = "";
-        while (($partial = socket_read($socket, 65384))) {
-            $response .= $partial;
+        if ($contentLength > 0) {
+            $socket->write("Content-Length: {$contentLength}" . Socket::EOL);
         }
-        socket_close($socket);
+
+        $socket
+            ->write(Socket::EOL)
+            ->write($request->getBody())
+            ->send(Socket::EOL);
+
+        var_dump($socket->read());
+        $socket->close();
+        die;
 
         $responseObject = handle($response);
         return new \GuzzleHttp\Promise\FulfilledPromise($responseObject);
     }
 
-    protected function createSocket()
-    {
-        $socket = socket_create(AF_UNIX, SOCK_STREAM, SOL_SOCKET);
-        if (!$socket) {
-            throw new \ErrorException("Cannot create socket");
-        }
-        if (!socket_connect($socket, $this->path)) {
-            throw new \ErrorException("Cannot connect socket to {$this->path}");
-        }
-        return $socket;
-    }
-
-    protected function writeToSocket($socket, $payload)
-    {
-
-        return $this
-    }
-
-    protected function closeSocket($socket)
-    {
-        socket_close($socket);
-    }
 }
 
-;
 
 $guzzle = new GuzzleHttp\Client([
     'handler' => new UnixSocketHandler('/tmp/socket.sock'),
 ]);
 
 echo $guzzle
-    ->post('https://limango.pl/test/123?query&123? hash i zażółć', ['json' => ['a' => 123]])
+    ->post('https://limango.pl/test/123?query&123? hash i zażółć', [
+        'json' => ['a' => 123],
+        'headers' => [
+            'Accept'     => 'application/json',
+        ]
+    ])
     ->getBody()->getContents();
 
 die;
